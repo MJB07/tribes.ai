2e3064322e2cf8a624d3c5c1c21cecdd76b2d56a
MJB07 <milap.patel07@gmail.com>
2019-02-05 18:33:29 +0530

    task complete

---
+++ Binary_Tree_Maximum_Path_Sum.py
@@ -0,0 +1,46 @@
+# Definition for a binary tree node.
+class TreeNode:
+     def __init__(self, x):
+         self.val = x
+         self.left = None
+         self.right = None
+
+class Solution:
+    def maxPathSum(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+                
+        self.max_value = float("-inf")
+        
+        self.maxPathResult(root)
+        
+        return self.max_value
+    
+    
+    def maxPathResult(self, root):
+        
+        if root == None:
+            
+             return 0
+            
+        l_sum = self.maxPathResult(root.left)
+        
+        r_sum = self.maxPathResult(root.right)
+        
+        if l_sum < 0 and r_sum < 0:
+            
+            self.max_value = max(self.max_value, root.val)
+            
+            return root.val
+        
+        if l_sum > 0 and r_sum > 0:
+            
+            self.max_value = max(self.max_value, root.val + l_sum + r_sum)
+            
+        maxFnl = max(l_sum, r_sum) + root.val
+        
+        self.max_value = max(self.max_value, maxFnl)
+        
+        return maxFnl        
---
+++ Wildcard_Matching.py
@@ -0,0 +1,44 @@
+class Solution:
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        
+        string, pattern = len(s), len(p)
+        
+        result_s, result_p = None,None
+        
+        s_i = p_i = 0
+        
+        while s_i < string:
+            
+            if p_i < pattern and (p[p_i] == "?" or s[s_i] == p[p_i]):
+                
+                s_i += 1
+                
+                p_i += 1
+                
+            elif p_i < pattern and p[p_i] == "*":
+                
+                result_s, result_p = s_i, p_i
+                
+                p_i += 1
+                
+            elif result_p is not None:
+                
+                s_i, p_i = result_s + 1, result_p
+                
+            else:
+                return False        
+            
+        while p_i < pattern:
+            
+            if p[p_i] != "*":
+                
+                return False
+            
+            p_i += 1
+            
+        return True            
